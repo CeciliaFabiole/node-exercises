1ca69334c59e85e6525221743fb82e5088a7b39a
ceci <ceciliafabiole18@gmail.com>
2022-12-01 18:05:06 +0200

    feat: add node-14

---
+++ src/Esercizio14/Challenge1.js
@@ -0,0 +1,5 @@
+const surprisingFact = "The bumblebee bat is the world's smallest mammal";
+
+console.log("SurprisingFatct:", surprisingFact)
+console.log({surprisingFact})
+console.log("surprisingFact = %S", surprisingFact)
---
+++ src/Esercizio14/Challenge3.js
@@ -0,0 +1,11 @@
+function importantTask() {
+    console.count("importantTaskCalls")
+}
+
+    importantTask();
+    importantTask();
+    importantTask();
+    importantTask();
+    console.countReset("importantTaskCalls")
+    importantTask();
+    importantTask();
